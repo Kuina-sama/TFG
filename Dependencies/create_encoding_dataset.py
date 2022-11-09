29f0004e66ae73cf17b18a1dd58d30f04a9f851e
Kuina <82050994+Kuina-sama@users.noreply.github.com>
2022-11-09 15:25:11 +0100

    Relative encoding y ConLLu con pos tag
    ConLLU file se puede generar de tal forma que aparezcan los pos tag (PENDIENTE: unificar funciones)
    
    Ahora tenemos como generar etiquetas para el relative encoding.

--- Dependencies/create_encoding_dataset.py
+++ Dependencies/create_encoding_dataset.py
@@ -1,5 +1,9 @@
 from CoDeLin.utils.reader import parse_conllu
+from CoDeLin.encs.enc_deps import *
+
 from supar import Parser
+
+import stanza
 
 import json
 
@@ -10,16 +14,20 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 parser = Parser.load('biaffine-dep-en')
 
 
-def create_conllu_file(dataset,output):
+def create_conllu_file(my_data,output):
 
-    """output must be the relative path to the output file, something like: r'./wizard_completo.conllu'"""
+    """
+    Recibe un dataset de Huggingface como entrada y realiza el parsing de dependencia de las oraciones.
+    
+    El resultado obtenido será guardado en formato conllu en la ruta indicada en el parametro output.
+    dataset: huggingface dataset. 
+    output must be the relative path to the output file, example: r'./wizard_completo.conllu'"""
 
 
 
-    my_data = {}
-    for key, value in enumerate(dataset['train']['text']):
-        my_data[key] = {'text': value, 'encoded_labels': None}
-
+    # my_data = {}
+    # for key, value in enumerate(data['train']['text']):
+    #     my_data[key] = {'text': value, 'encoded_labels': None}
 
     conllu_format={}
     for key in my_data:
@@ -37,14 +45,78 @@ def create_conllu_file(dataset,output):
 
             f.write(f'# text = {text} \n{conllu_item}\n')
 
-
+    print('Your ConLLU file has been created in: ', output)
+    # return
     return my_data
 
 
 
+nlp = stanza.Pipeline(lang='en',processors='tokenize,pos')
+def get_pos_tag(sentence):
 
-def create_parsing_labels_dataset(conllu_file,my_data,encoder):
+    # nlp = stanza.Pipeline(lang='en',processors='tokenize,pos')
 
+    doc = nlp(sentence)
+
+    pos_tags = [word.upos for sent in doc.sentences for word in sent.words ]
+
+    return pos_tags
+
+def create_conllu_file_with_pos(my_data,output):
+
+
+    ###################
+    # TO DO: Unir las dos funciones de create_conllu_filw
+    ###################
+    """Creates ConLLu file including pos_tag"""
+
+
+    # my_data = {}
+    # for key, value in enumerate(data['train']['text']):
+    #     my_data[key] = {'text': value, 'encoded_labels': None}
+
+    conllu_format={}
+    for key in my_data:
+        sentence = my_data[key]['text']
+
+
+        parsed_sentence= parser.predict(sentence,lang='en',verbose=False)[0]
+
+
+
+        parsed_sentence.values[3] = get_pos_tag(sentence)
+        conllu_format[key] = parsed_sentence
+        # print(f'Sentence number {key} added to conllu')
+
+    with open(output,'w',encoding='utf-8') as f:
+        for key in my_data:
+            text = my_data[key]['text']
+
+            conllu_item = conllu_format[key]
+
+            f.write(f'# text = {text} \n{conllu_item}\n')
+
+    print('Your ConLLU file has been created in: ', output)
+    return my_data
+
+
+def create_parsing_labels_dataset(conllu_file,my_data,encoding_type,separator):
+
+    """Takes a conllu file and encode it's dependencies.
+    Separator= separator used in your conllu file. Usuarlly _
+    Encoding_type:
+        -absolute: D_NaiveAbsoluteEncoding
+        -relative: D_NaiveRelativeEncoding
+        
+        in any other case it will use absolute encoding by default"""
+
+    if encoding_type == 'absolute':
+        encoder = naive_absolute.D_NaiveAbsoluteEncoding(separator)
+    elif encoding_type == 'relative':
+        encoder = naive_relative.D_NaiveRelativeEncoding(separator)
+    else:
+        print(encoding_type, ' is not a valid type.')
+        return 
 
     with open(conllu_file,'r',encoding='utf-8') as data_file:
         conllu_nodes = parse_conllu(data_file)
@@ -54,4 +126,3 @@ def create_parsing_labels_dataset(conllu_file,my_data,encoder):
 
 
     return my_data
-
